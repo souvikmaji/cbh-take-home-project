4dc06951165a088a20882285d0fecb0c500f5b03
souvikmaji <souvikmaji94@gmail.com>
2022-07-16 08:30:14 +0530

    untangeld if else

--- dpk.js
+++ dpk.js
@@ -10,22 +10,24 @@ exports.deterministicPartitionKey = (event) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
-
-  if (candidate) {
+  
+  if (event.partitionKey) {
+    candidate = event.partitionKey;
     if (typeof candidate !== "string") {
       candidate = JSON.stringify(candidate);
     }
-  } 
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+  } else {
+    const data = JSON.stringify(event);
+    candidate = createHash(data);
   }
+  
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+    candidate = createHash(candidate);
+  }
+
   return candidate;
 };
+
+const createHash = data => {
+  return crypto.createHash("sha3-512").update(data).digest("hex");
+}
